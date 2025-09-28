b694648f55eeaac4647b2127391ea49803159d1c
Rochelle Fernandes <149226541+Rochelle-Fernandes@users.noreply.github.com>
2025-09-28 15:01:52 +0530

    Update urls.py

--- Core/main/urls.py
+++ Core/main/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from .views import sentiment_timeline_api
 
 urlpatterns = [
 	path('', views.landing_page, name='landing_page'),
     path('information/', views.information_view, name='information_page'),
+]
+
+urlpatterns = [
+    path('sentiment-timeline/', sentiment_timeline_api, name='sentiment-timeline'),
+    # ... other endpoints ...
 ]
